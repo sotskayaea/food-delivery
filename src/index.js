5b74608587583dc9dbcfb372b6d476653d795591
sotskayaea <sotskaya.mentor@gmail.com>
2023-02-24 20:56:51 +0200

    add slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './js/modal.js';
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
